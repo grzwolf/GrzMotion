eeaa1d0ba96259320b3b6949c52b1221b6590789
grzwolf1 <grzwolf1@gmail.com>
2025-01-11 15:38:16 +0100

    update .net dependencies

--- RtspClientSharp/Properties/AssemblyInfo.cs
+++ RtspClientSharp/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GrzMotion")]
+[assembly: AssemblyTitle("RtspClientSharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GrzMotion")]
-[assembly: AssemblyCopyright("Copyright ©  2024, 2025")]
+[assembly: AssemblyProduct("RtspClientSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fde34ee6-430b-4516-977f-9e51301d5b5c")]
+[assembly: Guid("431891f7-9629-4ce0-b761-f54b4d06386e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
